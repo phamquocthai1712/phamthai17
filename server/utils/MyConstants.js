5f9d8f074175cf5a6c1a5938826ca381d34b23d6
phamquocthai1712 <128301494+phamquocthai1712@users.noreply.github.com>
2023-11-24 23:03:46 +0700

    get

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.o4iuax7.mongodb.net',
+    DB_USER: 'thai17',
+    DB_PASS: 'thai1712',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '123456789',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'phamthai171202@gmail.com', // gmail service
+    EMAIL_PASS: 'ooyonxeuocwusgjo'
+  };
+  module.exports = MyConstants;
